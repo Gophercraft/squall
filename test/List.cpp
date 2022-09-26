6c06635cc35a93d9212d10ce63c66b658eb528ef
Joshua Achorn <superp00t@tutanota.com>
2022-09-26 04:10:02 -0400

    chore: add tests for TSList::Combine

--- test/List.cpp
+++ test/List.cpp
@@ -1,6 +1,8 @@
 #include "storm/List.hpp"
 #include "test/Test.hpp"
 
+#include "Windows.h"
+
 struct TestListNode : TSLinkedNode<TestListNode> {
     uint32_t index = 0;
 };
@@ -113,5 +115,73 @@ TEST_CASE("TSList::Tail", "[list]") {
     SECTION("returns nullptr for uninitialized list") {
         STORM_LIST(TestListNode) list;
         REQUIRE(list.Tail() == nullptr);
+    }
+}
+
+TEST_CASE("TSList::Combine", "[list]") {
+    SECTION("combines after existing pointer") {
+        STORM_LIST(TestListNode) list1;
+        STORM_LIST(TestListNode) list2;
+        TestListNode* cursor;
+
+        for (uint32_t i = 0; i < 10; i++) {
+            TestListNode* node = list1.NewNode(2, 0, 0);
+            node->index = i;
+        }
+
+        cursor = list1.Head();
+        while (cursor != nullptr) {
+            cursor = cursor->Next();
+        }
+
+        for (uint32_t i = 500; i < 510; i++) {
+            TestListNode* node = list2.NewNode(2, 0, 0);
+            node->index = i;
+        }
+
+        list1.Combine(&list2, 1, nullptr);
+
+        cursor = list1.Head();
+        while (cursor != nullptr) {
+            printf("%d\n", cursor->index);
+            cursor = cursor->Next();
+        }
+        printf("End\n");
+
+        REQUIRE(list1.Head()->index == 500);
+        REQUIRE(list1.Tail()->index == 9);
+    }
+
+    SECTION("combines before existing pointer") {
+        STORM_LIST(TestListNode) list1;
+        STORM_LIST(TestListNode) list2;
+        TestListNode* cursor;
+
+        for (uint32_t i = 0; i < 10; i++) {
+            TestListNode* node = list1.NewNode(2, 0, 0);
+            node->index = i;
+        }
+
+        cursor = list1.Head();
+        while (cursor != nullptr) {
+            cursor = cursor->Next();
+        }
+
+        for (uint32_t i = 500; i < 510; i++) {
+            TestListNode* node = list2.NewNode(2, 0, 0);
+            node->index = i;
+        }
+
+        list1.Combine(&list2, 2, nullptr);
+
+        cursor = list1.Head();
+        while (cursor != nullptr) {
+            printf("%d\n", cursor->index);
+            cursor = cursor->Next();
+        }
+        printf("End\n");
+
+        REQUIRE(list1.Head()->index == 0);
+        REQUIRE(list1.Tail()->index == 509);
     }
 }
