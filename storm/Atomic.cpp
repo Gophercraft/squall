c5433b42883ad531973a8302b069721ff505f055
Joshua Achorn <superp00t@tutanota.com>
2022-09-26 03:39:43 -0400

    feat(Atomic): Add atomic comparison functions

--- storm/Atomic.cpp
+++ storm/Atomic.cpp
@@ -23,3 +23,33 @@ ATOMIC32 SInterlockedIncrement(ATOMIC32* ptr) {
     return __sync_fetch_and_add(ptr, 1) + 1;
 #endif
 }
+
+ATOMIC32 SInterlockedCompareExchange(ATOMIC32* destPtr, ATOMIC32 const& exchange, ATOMIC32 const& comperand) {
+#if defined(WHOA_SYSTEM_WIN)
+    return InterlockedCompareExchange(destPtr, exchange, comperand);
+#endif
+
+#if defined(WHOA_SYSTEM_MAC) || defined(WHOA_SYSTEM_LINUX)
+    __sync_val_compare_and_swap(destPtr, comperand, exchange);
+#endif
+}
+
+ATOMIC64 SInterlockedCompareExchange(ATOMIC64* destPtr, ATOMIC64 const& exchange, ATOMIC64 const& comperand) {
+#if defined(WHOA_SYSTEM_WIN)
+    return InterlockedCompareExchange64(destPtr, exchange, comperand);
+#endif
+
+#if defined(WHOA_SYSTEM_MAC) || defined(WHOA_SYSTEM_LINUX)
+    __sync_val_compare_and_swap(destPtr, comperand, exchange);
+#endif
+}
+
+void* SInterlockedCompareExchangePointer(void** destPtr, void* exchange, void* comperand) {
+#if defined(WHOA_SYSTEM_WIN)
+    return InterlockedCompareExchangePointer(destPtr, exchange, comperand);
+#endif
+
+#if defined(WHOA_SYSTEM_MAC) || defined(WHOA_SYSTEM_LINUX)
+    __sync_val_compare_and_swap(destPtr, comperand, exchange);
+#endif
+}
--- storm/Atomic.hpp
+++ storm/Atomic.hpp
@@ -4,15 +4,25 @@
 #include <cstdint>
 
 #if defined(WHOA_SYSTEM_WIN)
+#include <windows.h>
+
 typedef long ATOMIC32;
+typedef LONG64 ATOMIC64;
 #endif
 
 #if defined(WHOA_SYSTEM_MAC) || defined(WHOA_SYSTEM_LINUX)
 typedef int32_t ATOMIC32;
+typedef int64_t ATOMIC64;
 #endif
 
 ATOMIC32 SInterlockedDecrement(ATOMIC32* ptr);
 
 ATOMIC32 SInterlockedIncrement(ATOMIC32* ptr);
+
+ATOMIC32 SInterlockedCompareExchange(ATOMIC32* destPtr, const ATOMIC32& exchange, const ATOMIC32& comperand);
+
+ATOMIC64 SInterlockedCompareExchange(ATOMIC64* destPtr, const ATOMIC64& exchange, const ATOMIC64& comperand);
+
+void* SInterlockedCompareExchangePointer(void** destPtr, void* exchange, void* comperand);
 
 #endif
